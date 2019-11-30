bf6379caeb190a4b3f48fdb86b0b9216206b655d
mehtapaydin <mehtapaydinnnn@gmail.com>
2019-11-29 22:52:21 -0500

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,10 @@ const HomePage = () => {
   return (
       <Layout>
         <div className={style.narrow}>
-        <h1 className={style.h1}> Hi, I am Mehtap, an Designer, Creative Technologist based in New York</h1>
+        <h1 className={style.h1}> Hi, I am Mehtap, a Designer, Creative Technologist based in New York</h1>
         <p>
-        I'm currently working as an Interaction Designer at Google, creating solutions for Google Cloud products and services.
-        While I am working on my portfolio you can check out some of my glitches and memories. 
-      
+        I'm currently working as an Interaction Designer at Google, creating solutions for Google Cloud products and services. 
+        While I am working on my portfolio you can check out some of my <a href="https://glitch.com/@mehtapaydin" target="_blank">glitches </a> ,<a href="https://vsco.co/uzgunumleyla" target="_blank">photographs </a>
         </p>
        </div>
       </Layout>
