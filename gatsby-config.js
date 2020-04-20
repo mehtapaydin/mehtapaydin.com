2a5bba2c8f4507814ef2db33e4d6d35cd6728ffb
Mehtap Aydin <mehtapaydinnnn@gmail.com>
2020-04-19 21:08:21 -0400

    Image render from Markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,24 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-relative-images`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 750,
+              linkImagesToOriginal: false
+            },
+          },
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
 
             
@@ -35,8 +53,7 @@ module.exports = {
           path: `${__dirname}/src/`
       }
   },
-  'gatsby-transformer-remark'
-
+ 
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
--- src/posts/imox.md
+++ src/posts/imox.md
@@ -5,6 +5,8 @@ thumbnail: ./imox01.png
 description: IMOX (EE-MOH-SH) A 360-IMMERSIVE EXPERIENCE ABOUT IMMIGRATION. BECOME A DROP OF WATER AND TRAVEL THROUGH TIME
 ---
 
-### Lorem Ipsum
+### Project background
+
+![IMOX](./imox01.png)
 
 I am IMOX
